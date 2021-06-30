4ef20244a1fbaf85d5f7c6a129ed27e6b1df2007
Ces95 <c-diazperez95>
2021-06-29 20:26:16 -0400

    cc

--- test/anagram.test.js
+++ test/anagram.test.js
@@ -1,6 +1,11 @@
-import {Anagramas} from "../src/app";
+import {Anagramas} from "../src/app.ts";
+
+
+
 test('original implementation', () => {
-  // now we have the original implementation,
-  // even if we set the automocking in a jest configuration
+  
+  
   expect(Anagramas.checkAnagrams()).toBe('token');
+  
 });
+
